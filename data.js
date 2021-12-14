95eda9e567c90800ac6e695924adeff463b79b00
ismailhaque <ismailhaque2956@gmail.com>
2021-12-14 23:30:24 +0600

    upload all work

--- data.js
+++ data.js
@@ -1,7 +1,356 @@
-let students = localStorage.getItem('student');
+const devs = [
+
+    {
+        id          : 01,
+        name        : `Md Ismail`,
+        age         : 60,
+        skill       : `MERN stack`,
+        location    : `Jibonpur`,
+        income      : 1000,
+        gender      : `male`,
+    },
+    {
+        id          : 02,
+        name        : `Md Saikat`,
+        age         : 50,
+        skill       : `Laravel`,
+        location    : `Panchbibi`,
+        income      : 950,
+    },
+    {
+        id          : 03,
+        name        : `Md NAimul`,
+        age         : 20,
+        skill       : `Django`,
+        location    : `Joypurhat`,
+        income      : 900,
+        gender      : `male`,
+    },
+    {
+        id          : 04,
+        name        : `Md Minhaj`,
+        age         : 50,
+        skill       : `Golan`,
+        location    : `Majhina`,
+        income      : 600,
+    },
+    {
+        id          : 05,
+        name        : `Md Sifat`,
+        age         : 40,
+        skill       : `WordPress`,
+        location    : `Bagjana`,
+        income      : 550,
+        gender      : `male`,
+    },
+    {
+        id          : 06,
+        name        : `Md SEtu`,
+        age         : 30,
+        skill       : `java`,
+        location    : `Jibonpur`,
+        income      : 800,
+        gender      : `male`,
+    },
+];
+
+const students = [
+
+    {
+        roll        : 1,
+        name        : 'Md Saikat',
+        age         : 15, 
+        location    : 'JIbonpur',
+        gender      : 'Male',
+        bn          : 87,
+        en          : 90, 
+        math        : 78,
+        s           : 87, 
+        ss          : 23, 
+        rel         : 95
+    },
+    {
+        roll        : 2,
+        name        : 'Md Srabon',
+        age         : 14, 
+        location    : 'Majhina',
+        gender      : 'Male',
+        bn          : 67,
+        en          : 40, 
+        math        : 78,
+        s           : 87, 
+        ss          : 90, 
+        rel         : 80
+    },
+    {
+        roll        : 3,
+        name        : 'Md Sifat',
+        age         : 15, 
+        location    : 'Angra',
+        gender      : 'Male',
+        bn          : 20,
+        en          : 90, 
+        math        : 78,
+        s           : 87, 
+        ss          : 23, 
+        rel         : 40
+    },
+    {
+        roll        : 4,
+        name        : 'Md Ismail',
+        age         : 17, 
+        location    : 'Jibonpur',
+        gender      : 'Male',
+        bn          : 67,
+        en          : 90, 
+        math        : 78,
+        s           : 87, 
+        ss          : 95, 
+        rel         : 80
+    },
+    {
+        roll        : 5,
+        name        : 'Md Sabbir',
+        age         : 16, 
+        location    : 'Jibonpur',
+        gender      : 'Male',
+        bn          : 67,
+        en          : 40, 
+        math        : 28,
+        s           : 87, 
+        ss          : 90, 
+        rel         : 80
+    },
+    {
+        roll        : 6,
+        name        : 'Md Setu',
+        age         : 15, 
+        location    : 'Uttara',
+        gender      : 'Male',
+        bn          : 20,
+        en          : 90, 
+        math        : 78,
+        s           : 37, 
+        ss          : 23, 
+        rel         : 80
+    },
+    {
+        roll        : 7,
+        name        : 'Md Sadakatul',
+        age         : 16, 
+        location    : 'Majhina',
+        gender      : 'Male',
+        bn          : 67,
+        en          : 50, 
+        math        : 78,
+        s           : 87, 
+        ss          : 23, 
+        rel         : 80
+    },
+    {
+        roll        : 8,
+        name        : 'Md Naimul',
+        age         : 14, 
+        location    : 'Angra',
+        gender      : 'Male',
+        bn          : 67,
+        en          : 40, 
+        math        : 78,
+        s           : 87, 
+        ss          : 90, 
+        rel         : 90
+    },
+    {
+        roll        : 9,
+        name        : 'Ms. Munni',
+        age         : 13, 
+        location    : 'Jibonpur',
+        gender      : 'Female',
+        bn          : 20,
+        en          : 90, 
+        math        : 78,
+        s           : 87, 
+        ss          : 23, 
+        rel         : 80
+    },
+    {
+        roll        : 9,
+        name        : 'Ms. Ranu',
+        age         : 14, 
+        location    : 'Majhina',
+        gender      : 'Female',
+        bn          : 80,
+        en          : 90, 
+        math        : 78,
+        s           : 87, 
+        ss          : 33, 
+        rel         : 80
+    },
+    {
+        roll        : 9,
+        name        : 'Ms. Ayesha',
+        age         : 18, 
+        location    : 'Angra',
+        gender      : 'Female',
+        bn          : 40,
+        en          : 90, 
+        math        : 78,
+        s           : 87, 
+        ss          : 53, 
+        rel         : 80
+    }
+
+];
+
+const phoneBooks = [
+    {
+           name     : 'Naimul',
+           mobileNO : '01768876312',
+           relation : 'Brother',
+           location : 'Hili'
+    },
+    {
+        name     : 'Rezwan',
+        mobileNO : '01768876312',
+        relation : 'Bhai',
+        location : 'Hili'
+    },
+    
+    {
+        name     : 'Talha',
+        mobileNO : '01768876312',
+        relation : 'Bhai',
+        location : 'Hili'
+    },
+    
+    {
+        name     : 'Khalid',
+        mobileNO : '01768876312',
+        relation : 'Friend',
+        location : 'Hili'
+    },
+    {
+        name     : 'Hasan',
+        mobileNO : '01768876312',
+        relation : 'Brother',
+        location : 'Hili'
+  },
+
+  {
+    name     : 'Khalek',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Malek',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Ashik',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Ridoy',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Kamrul',
+    mobileNO : '01768876312',
+    relation : 'Uncle',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Moni',
+    mobileNO : '01768876312',
+    relation : 'Uncle',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Mamun',
+    mobileNO : '01768876312',
+    relation : 'Unle',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Masud',
+    mobileNO : '01768876312',
+    relation : 'Uncle',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Siyam',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Atik',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+ },
+ 
+ {
+    name     : 'Nasim',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Hasem',
+    mobileNO : '01768876312',
+    relation : 'Uncle',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Latif',
+    mobileNO : '01768876312',
+    relation : 'Uncle',
+    location : 'Hili'
+ },
+
+ {
+    name     : 'Amerul',
+    mobileNO : '01768876312',
+    relation : 'Brother',
+    location : 'Hili'
+  },
+
+ {
+    name     : 'Refaz',
+    mobileNO : '01768876312',
+    relation : 'Uncle',
+    location : 'Hili'
+ } 
+];
+
+localStorage.setItem("dev",JSON.stringify(devs));
+localStorage.setItem("student",JSON.stringify(students));
+localStorage.setItem("phonebook",JSON.stringify(phoneBooks));
+
+
+let student = localStorage.getItem('student');
 let dev = localStorage.getItem('dev');
 let phonebook = localStorage.getItem('phonebook');
 
-console.log(JSON.parse(students));
+console.log(JSON.parse(student));
 console.log(JSON.parse(dev));
 console.log(JSON.parse(phonebook));
